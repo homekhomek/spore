8f4629ca94c3b5cf9181d4a01c8606caf829621b
FunATuns <payton.obrycki@gmail.com>
2018-05-29 20:28:42 -0500

    even more register shit
    added hook and emit to main.js, also documented in the comments how its going to work so you can develop for it <3

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,13 @@
 var socket = io.connect(); // take your ip out for saftey when pushing
 
+socket.emit("Register",{username: "", password: ""}); // once emited will return with a packets also named Register (see below)
+
+socket.on("Register",function (data) {
+    /*data will be one of few things
+        {
+            status: "success/fail"
+            type: "" //if fail, will be either "usernameNotUnique", "hasWhiteSpace", or "tooLong"
+            key: "" //if success, will return key so you can log the user in
+        }
+    */
+});
