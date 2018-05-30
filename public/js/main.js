de5398c57b6267ccabbfcaebe9073c45e8c51e07
FunATuns <payton.obrycki@gmail.com>
2018-05-29 19:43:12 -0500

    Fixed merge issue
    Lol I didn't realize you messed with the file system

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,2 @@
 var socket = io.connect(); // take your ip out for saftey when pushing
 
-<<<<<<< HEAD:public/main.js
-
-=======
-socket.emit('Joined',null);
->>>>>>> 7a9775571d03e3aa242395ee4eef785b91d72a58:public/js/main.js
