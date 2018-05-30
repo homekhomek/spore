a165dbe45775fc48c49729e4aa3ac871ae75c9a5
FunATuns <payton.obrycki@gmail.com>
2018-05-29 21:04:24 -0500

    heres the property bren
    much love

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ var socket = io.connect(); // take your ip out for saftey when pushing
 
 socket.emit("Register",{username: "", password: "", email: ""}); // once emited will return with a packets also named Register (see below)
 
+//window.navigator.standalone 
+
 socket.on("Register",function (data) {
     /*data will be one of few things
         {
