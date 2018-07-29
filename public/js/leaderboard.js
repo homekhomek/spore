b93c2d7bcc90fc5bfeb96d4367d713c527498ebb
Noise859 <brennan.neuser@gmail.com>
2018-07-29 16:47:32 -0500

    Leaderboard stuff also
    Restyle leaderboard
    new dropdown
    category changing

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,7 +1,11 @@
 socket.emit("Leaderboard", "overall");
 socket.on("Leaderboard", function(inf){
   console.log(inf);
+  document.getElementById("board").innerHTML = "";
   for(var i = 0; i < inf.length; i++) {
     document.getElementById("board").innerHTML += "<div class='boardpiece' onclick='window.location=\"profile.html?username=" + inf[i].username +"\"'><h1 class='pieceplace'>" + inf[i].place + "</h1><img class='pieceimg' src='" + getAvatarURL(inf[i].username) + "'><div class='pieceusername'>" + inf[i].username + "</div><div class='piecescore'>" + inf[i].score + "</div></div>";
   }
+});
+$("#catpick").change(function(){
+  socket.emit("Leaderboard", this.value);
 });
